cb3d11ade24f868e57b6e9db65bd31bb2848de8f
Jaakko Haavisto <jaako.haavisto@kolumbus.fi>
2017-06-06 11:06:28 +0300

    bang-a-rang

---
+++ Assets/Scripts/ShurikenRotator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShurikenRotator : MonoBehaviour {
+
+    public float rotSpeed = 1;
+	
+	void Update () {
+        GetComponentInChildren<Transform>().Rotate(0, 0, rotSpeed * Time.deltaTime);
+	}
+}
