7661e40b58b99e9faa8ac1a040b4ce09c78dcef5
Jaakko Haavisto <jaako.haavisto@kolumbus.fi>
2017-06-08 14:32:31 +0300

    UI back in town. aloitettu siis. taas.

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -42,11 +42,11 @@ public class Player : MonoBehaviour {       // gonan 2d actual
 
     public int hp;
     public int playerLives = 3;
+    public int secondaryAmmo = 99;
 
     bool facingRight;
     bool canMove;
     bool jump;
-    bool knockBackActive;
     bool crouchWhip;
     public bool onStair;
     public bool whipping;
@@ -74,7 +74,6 @@ public class Player : MonoBehaviour {       // gonan 2d actual
     public float jointStep = 0.25f;
     public float jointMaxDist = 4;
     public float jointMinDist = 0.5f;
-    public Slider playerHealthBar;
     PolygonCollider2D polCollider;
     public SpriteRenderer spriteRenderer;
     GameManager gm;
@@ -88,10 +87,8 @@ public class Player : MonoBehaviour {       // gonan 2d actual
     public GameObject shurikenPrefab;
     public GameObject[] shurikens;
 
-    public int secondaryAmmo = 99;
 
     DBController dbc;
-
     public float gravity = 1;
     public bool canTakeDamage = true;
     bool lastFaceDirRight;
@@ -114,7 +111,7 @@ public class Player : MonoBehaviour {       // gonan 2d actual
         canMove = true;
         joint.enabled = false;
         line.enabled = false;
-        playerHealthBar.value = hp;
+        // set player/enemy health info     FIX THIS   FIX THIS   FIX THIS   FIX THIS   FIX THIS   FIX THIS   FIX THIS   FIX THIS   FIX THIS   FIX THIS   FIX THIS   
     }
 
     public void FallTrigger() {
@@ -144,7 +141,7 @@ public class Player : MonoBehaviour {       // gonan 2d actual
         //print("enemy hit player");
         if (!canTakeDamage) return; 
         hp--;
-        playerHealthBar.value = hp;
+        // set player health info     FIX THIS   FIX THIS   FIX THIS   FIX THIS   FIX THIS   FIX THIS   FIX THIS   FIX THIS   FIX THIS   FIX THIS   FIX THIS   
         if (hp == 0) {
             Death();
         } else {
@@ -227,7 +224,6 @@ public class Player : MonoBehaviour {       // gonan 2d actual
         if (currentState == PlayerState.KnockedBack) return;        
         canWhip = false;
         knockBackDir = dir;
-        knockBackActive = true;
         tickTime = 0;
         if (currentState != PlayerState.Dead && currentState != PlayerState.IndianaJones && currentState != PlayerState.OnStair) {
             currentState = PlayerState.KnockedBack;
@@ -262,7 +258,6 @@ public class Player : MonoBehaviour {       // gonan 2d actual
             canMove = true;
             canWhip = true;
         }
-        knockBackActive = false;
     }
 
     public void StopWhip() {
@@ -341,28 +336,17 @@ public class Player : MonoBehaviour {       // gonan 2d actual
         if (currentState == PlayerState.IndianaJones) {
             dbc.IndianaJones();
             Vector3 hookPos = new Vector3(currentHookPoint.transform.position.x, currentHookPoint.transform.position.y - 0.2f, 0);
-
             line.SetPosition(0, hookDistCheck.position);
             line.SetPosition(1, hookPos);
             if (hookDistCheck.position.y < hookPos.y - 1) {
-                //rb.velocity = new Vector3(horizontalAxis * (swingSpeed - Mathf.Abs(transform.position.x - hookPos.x) * swingReducer), rb.velocity.y, 0);
-                //rb.velocity += new Vector2(horizontalAxis * (swingSpeed - Mathf.Abs(transform.position.x - hookPos.x) * swingReducer), rb.velocity.y);
                 var left = Vector3.Cross((hookDistCheck.position - hookPos), Vector3.forward).normalized;
                 rb.AddForce(-horizontalAxis * swingSpeed * left);
                 print(horizontalAxis * swingSpeed * left.normalized);
-                //Debug.DrawLine(hookDistCheck.position, hookDistCheck.position + left.normalized * 3);
-                //Debug.DrawLine(hookDistCheck.position, hookDistCheck.position + (Vector3)rb.velocity * 3);
-                //var dampVelocity = -Vector3.Project(rb.velocity, left).normalized;
-                //rb.velocity -= Vector3.ClampMagnitude(dampVelocity* Time.deltaTime;
                 var mag = rb.velocity.magnitude;
                 var newMag = mag - swingReducer * Time.deltaTime;
                 newMag = Mathf.Clamp(newMag, 0f, newMag);
                 rb.velocity = rb.velocity.normalized * newMag;
             }
-            /*else {
-                // ???
-                rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, 0);
-            }*/
             float dist;
             dist = joint.distance;
 
