e408a5a0b7cfa9526c990bf17942d21677cdd215
Jaakko Haavisto <jaako.haavisto@kolumbus.fi>
2017-06-09 10:21:27 +0300

    bobtheblob jatkuu

--- Assets/Scripts/BobTheBlob.cs
+++ Assets/Scripts/BobTheBlob.cs
@@ -4,15 +4,80 @@ using UnityEngine;
 
 public class BobTheBlob : MonoBehaviour {
 
-	void Start () {
-		
-	}
-	
-    public void DropBlob() {
+    bool start;
+    float tickTime;
+    public float timer = 5;
+    public float speedH = 1;
+    public float speedV = -0.1f;
+    FabricCtrl fabCtrl;
+    bool flash;
 
+    private void Start() {
+        fabCtrl = GameObject.Find("FabricCtrl").GetComponent<FabricCtrl>();
+        GetComponent<SpriteRenderer>().enabled = false;
+        GetComponent<CircleCollider2D>().enabled = false;
+        GetComponentInChildren<ParticleSystem>().enableEmission = false;
     }
 
-	void Update () {
-		
-	}
+    public void DropBlob() {
+        start = true;
+        GetComponent<SpriteRenderer>().enabled = true;
+        GetComponent<CircleCollider2D>().enabled = true;
+        GetComponentInChildren<ParticleSystem>().enableEmission = true;
+    }
+
+    void GoRight() {
+        transform.position += new Vector3(speedH * 1, speedV, 0) * Time.deltaTime;
+    }
+
+    void GoLeft() {
+        transform.position += new Vector3(speedH * -1, speedV, 0) * Time.deltaTime;
+    }
+
+
+
+    void Update() {
+        if (start) {
+            tickTime += Time.deltaTime;
+            if (tickTime >= timer) {
+                start = false;
+                GetComponent<SpriteRenderer>().enabled = false;
+                GetComponent<CircleCollider2D>().enabled = false;
+                GetComponentInChildren<ParticleSystem>().enableEmission = false;
+            }
+            if (tickTime <= 0.38f) {
+                GoRight();
+            } else if (tickTime <= 1) {
+                GoLeft();
+            } else if (tickTime <= 1.75) {
+                GoRight();
+            } else if (tickTime <= 2.25) {
+                GoLeft();
+            } else if (tickTime < 3) {
+                GoRight();
+            } else if (tickTime < timer) {
+                GoLeft();
+            }
+            if (tickTime > 4.5f) {
+                if (flash) {
+                    GetComponent<SpriteRenderer>().enabled = true;
+                    flash = false;
+                } else {
+                    GetComponent<SpriteRenderer>().enabled = false;
+                    flash = true;
+                }
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D c) {
+        if (c.tag == "Player" && start) {
+            fabCtrl.PlaySoundPickup();
+            GetComponent<SpriteRenderer>().enabled = false;
+            GetComponent<CircleCollider2D>().enabled = false;
+            GetComponentInChildren<ParticleSystem>().enableEmission = false;
+            start = false;
+            gameObject.SetActive(false);
+        }
+    }
 }
