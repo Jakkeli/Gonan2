1d9d278cbaf54bb092395e1b9307b8d15dae88ca
Jaakko Haavisto <jaako.haavisto@kolumbus.fi>
2017-06-09 09:36:50 +0300

    bob the blob aloitettu

---
+++ Assets/Scripts/BobTheBlob.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BobTheBlob : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+    public void DropBlob() {
+
+    }
+
+	void Update () {
+		
+	}
+}
