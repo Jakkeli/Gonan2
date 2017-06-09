1f5cea12811e91f89d4bdd8f0ae9a0b93923a2a3
Jaakko Haavisto <jaako.haavisto@kolumbus.fi>
2017-06-09 12:29:03 +0300

    UI edistyy

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -10,18 +10,30 @@ public class GameManager : MonoBehaviour {
 
     public GameState currentState;
     public GameLevel currentLevel;
-    public Text levelLivesAmmo;
+    public Text scoreText;
+    public Text blockLivesAmmoTimerText;
+
     string level = "LEVEL 1-1";
     string playerLives = "LIVES 3";
     string secondaryAmmo = "PLAYER 00";
+    string scoreNumbers;
 
 
     Player player;
+    public int score;
 
+    public Transform[] playerHealthBars;
+    public Transform[] enemyHealthBars;
+
+    public Sprite emptyBar;
+    public Sprite FilledBar;
+    public int playerHealth;
+    public int enemyHealth;
 
     void Start () {
         player = GameObject.Find("player").GetComponent<Player>();
         UpdateLevelLivesAmmo();
+        scoreText.text = "SCORE    " + score + "\n" + "    ";
 	}
 
     public void UpdateLevelLivesAmmo() {
@@ -34,6 +46,25 @@ public class GameManager : MonoBehaviour {
         //levelLivesAmmo.text = level + "\n" + playerLives + "\n" + secondaryAmmo;
     }
 
+    public void UpdateScore() {
+
+    }
+
+    public void UpdatePlayerEnemyHealth(int ph, int eh) {
+        for (int i = 0; i < ph; i++) {
+            playerHealthBars[i].GetComponent<Image>().sprite = FilledBar;
+        }
+        for (int i = ph; i < 16; i++) {
+            playerHealthBars[i].GetComponent<Image>().sprite = emptyBar;
+        }
+        for (int i = 0; i < eh; i++) {
+            enemyHealthBars[i].GetComponent<Image>().sprite = FilledBar;
+        }
+        for (int i = eh; i < 16; i++) {
+            enemyHealthBars[i].GetComponent<Image>().sprite = emptyBar;
+        }
+    }
+
     public void GameOver() {
         print("Game Over N00b!1! git gud.");
         currentState = GameState.GameOver;
@@ -44,8 +75,8 @@ public class GameManager : MonoBehaviour {
             UnityEngine.SceneManagement.SceneManager.LoadScene(0);
         }
 
-        if (Input.GetKeyDown(KeyCode.C)) {
-            GameObject.Find("player").GetComponent<Player>().StopWhip();
+        if (Input.GetKeyDown(KeyCode.U)) {
+            UpdatePlayerEnemyHealth(playerHealth, enemyHealth);
         }
     }
 }
