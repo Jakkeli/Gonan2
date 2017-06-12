59964bedaa71902d5397fc9bc096fe98764cc37f
Jaakko Haavisto <jaako.haavisto@kolumbus.fi>
2017-06-12 11:52:46 +0300

    boss kehittyy

---
+++ Assets/Scripts/BossOne.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BossState { Inactive, Triggered, Fighting, Dead}
+
+public class BossOne : MonoBehaviour {
+
+    public BossState currentState;
+    public int hp;
+    GameObject player;
+    Player ps;
+    GameManager gm;
+    Vector3 playerPos;
+    bool inPlace;
+    bool triggered;
+    public float transitionSpeed = 1;
+
+    float targetX = 289;
+
+	void Start () {
+        player = GameObject.Find("player");
+        ps = player.GetComponent<Player>();
+        gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+	}
+
+    public void TriggerFight() {
+        //move to fightPos
+        currentState = BossState.Triggered;
+    }
+
+    public void TakeDamage() {
+        if (currentState != BossState.Fighting) return;
+        hp--;
+        if (hp == 0) {
+            Death();
+        }
+    }
+
+    void Death() {
+
+    }
+
+    void ShootNormal() {
+
+    }
+
+    void Laugh() {
+
+    }
+	
+	void Update () {
+
+
+		if (currentState == BossState.Triggered) {
+            transform.position += new Vector3(-transitionSpeed * Time.deltaTime, 0, 0);
+            if (transform.position.x <= targetX) currentState = BossState.Fighting;
+        }
+	}
+}
